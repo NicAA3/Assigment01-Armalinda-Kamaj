d7b3e8262752264310e736df8a77d233c6e96b6a
Armalinda Kamaj <Armalindakamaj@gmail.com>
2024-09-10 12:23:18 +0200

    Second commit, uppdated code

--- tests/testsuite01.spec.ts
+++ tests/testsuite01.spec.ts
@@ -19,7 +19,7 @@ test.beforeEach(async ({ page }) => {
   await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
 
 });
-/*
+
 test.afterEach(async ({ page }) => {
   const dashboardPage = new DashboardPage(page);
   await dashboardPage.performLogout()
@@ -31,7 +31,7 @@ test.afterEach(async ({ page }) => {
 
 });
 
-*/
+
 test.describe("Test suite 01", () => {
 
   test("TC 01A", async ({ page }) => {
@@ -54,80 +54,80 @@ test.describe("Test suite 01", () => {
     await page.waitForTimeout(5000);
 
   });
-  /** 
-    test("TC 02-create room", async ({ page }) => {
-  
-      const createRoomPage = new CreateRoomPage(page);
-      //Enter to room page
-      await createRoomPage.goToViewRoom();
-      await expect(page.getByText('Rooms')).toBeVisible()
-      //Enter to Create room Form
-      await createRoomPage.goToCreateRoom();
-      await expect(page.getByText('New Room')).toBeVisible();
-  
-      // Fill out the form to create a new room
-      const category = await createRoomPage.selectCategory.inputValue();
-      const roomNumber = await createRoomPage.selectNumber.inputValue();
-      const floor = await createRoomPage.selectFloor.inputValue();
-      const price = await createRoomPage.selectPrice.inputValue();
-      const features = await createRoomPage.selectFeatures.inputValue();
-  
-      //Create a new room
-      await createRoomPage.fillOutCreateRoomsForm();
-  
-      // Get the last created room element
-      const lastRoomElement = page.locator("#app > div > div.rooms > div:nth-last-child(1)");
-  
-      // Assertions 
-      await expect(lastRoomElement).toContainText(category);
-      await expect(lastRoomElement).toContainText(roomNumber);
-      await expect(lastRoomElement).toContainText(floor);
-      await expect(lastRoomElement).toContainText('Available');
-      await expect(lastRoomElement).toContainText(price);
-      await expect(lastRoomElement).toContainText(features);
-  
-      await page.waitForTimeout(5000);
-    });
-  
-  
-    test("TC 03-create new client", async ({ page }) => {
-  
-      const createNewClientPage = new CreateNewClientPage(page)
-  
-      await createNewClientPage.createNewClientForm()
-      const element = page.locator(
-        "#app > div > div.clients > div:nth-last-child(1)"
-      );
-      // Assertions 
-      await expect(element).toContainText(createNewClientPage.fullName);
-      await expect(element).toContainText(createNewClientPage.userEmail);
-      await expect(element).toContainText(createNewClientPage.userPhoneNo);
-      await page.waitForTimeout(5000);
-  
-    });
-  
-    test("TC 04-create new bills", async ({ page }) => {
-  
-      const createBillsPage = new CreateBillsPage(page)
-      //go to Create bill page
-      await createBillsPage.goToViewBills()
-      await expect(page.getByText('Bills')).toBeVisible;
-      //Create bill page-navigate
-      await createBillsPage.goToCreateBills()
-      await expect(page.getByText('New Bill')).toBeVisible();
-  
-  
-      const filledValue = await createBillsPage.fillNewBillForm();
-      const element = page.locator("#app > div > div.bills > div:nth-last-child(1)");
-  
-      // Assertions 
-      await expect(element).toContainText("ID");
-      await expect(element).toContainText(filledValue);
-  
-      await page.waitForTimeout(5000);
-  
-    });
-  */
+
+  test("TC 02-create room", async ({ page }) => {
+
+    const createRoomPage = new CreateRoomPage(page);
+    //Enter to room page
+    await createRoomPage.goToViewRoom();
+    await expect(page.getByText('Rooms')).toBeVisible()
+    //Enter to Create room Form
+    await createRoomPage.goToCreateRoom();
+    await expect(page.getByText('New Room')).toBeVisible();
+
+    // Fill out the form to create a new room
+    const category = await createRoomPage.selectCategory.inputValue();
+    const roomNumber = await createRoomPage.selectNumber.inputValue();
+    const floor = await createRoomPage.selectFloor.inputValue();
+    const price = await createRoomPage.selectPrice.inputValue();
+    const features = await createRoomPage.selectFeatures.inputValue();
+
+    //Create a new room
+    await createRoomPage.fillOutCreateRoomsForm();
+
+    // Get the last created room element
+    const lastRoomElement = page.locator("#app > div > div.rooms > div:nth-last-child(1)");
+
+    // Assertions 
+    await expect(lastRoomElement).toContainText(category);
+    await expect(lastRoomElement).toContainText(roomNumber);
+    await expect(lastRoomElement).toContainText(floor);
+    await expect(lastRoomElement).toContainText('Available');
+    await expect(lastRoomElement).toContainText(price);
+    await expect(lastRoomElement).toContainText(features);
+
+    await page.waitForTimeout(5000);
+  });
+
+
+  test("TC 03-create new client", async ({ page }) => {
+
+    const createNewClientPage = new CreateNewClientPage(page)
+
+    await createNewClientPage.createNewClientForm()
+    const element = page.locator(
+      "#app > div > div.clients > div:nth-last-child(1)"
+    );
+    // Assertions 
+    await expect(element).toContainText(createNewClientPage.fullName);
+    await expect(element).toContainText(createNewClientPage.userEmail);
+    await expect(element).toContainText(createNewClientPage.userPhoneNo);
+    await page.waitForTimeout(5000);
+
+  });
+
+  test("TC 04-create new bills", async ({ page }) => {
+
+    const createBillsPage = new CreateBillsPage(page)
+    //go to Create bill page
+    await createBillsPage.goToViewBills()
+    await expect(page.getByText('Bills')).toBeVisible;
+    //Create bill page-navigate
+    await createBillsPage.goToCreateBills()
+    await expect(page.getByText('New Bill')).toBeVisible();
+
+
+    const filledValue = await createBillsPage.fillNewBillForm();
+    const element = page.locator("#app > div > div.bills > div:nth-last-child(1)");
+
+    // Assertions 
+    await expect(element).toContainText("ID");
+    await expect(element).toContainText(filledValue);
+
+    await page.waitForTimeout(5000);
+
+  });
+
   test("TC 05-create new reservation", async ({ page }) => {
 
     const createReservationPage = new CreateReservationsPage(page)
@@ -141,10 +141,11 @@ test.describe("Test suite 01", () => {
     await createReservationPage.fillNewReservationForm()
     //const filledValue = await createReservationPage.fillNewReservationForm()
     const element = page.locator('#app > div > div.reservations > div:nth-last-child(1)')
-    await expect(element).toContainText('Client:1')
-    await expect(element).toContainText('Room:1')
-    await expect(element).toContainText('Bill:1')
     await expect(element).toContainText('Jonas Hellman')
+    await expect(element).toContainText('Client: 1')
+    await expect(element).toContainText('Room: 1')
+    await expect(element).toContainText('Bill: 1')
+
     //await expect(element).toContainText(filledValue);
 
     await page.waitForTimeout(5000);
